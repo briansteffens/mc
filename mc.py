fa0ebd4bf6e0ed5725b4adb9c549d22062a7a426
Brian Steffens <briansteffens@gmail.com>
2016-06-26 16:20:15 -0400

    Make strtab generation more dynamic

--- mc.py
+++ mc.py
@@ -119,7 +119,8 @@ class SectionHeader(object):
 class SymbolTableEntry(object):
 
     def __init__(self):
-        self.name = 0
+        self.name = None
+        self.name_offset = 0
         self.info = STT_NOTYPE
         self.other = STV_DEFAULT
         self.section_header_index = 0
@@ -148,7 +149,7 @@ class SymbolTableEntry(object):
     def to_bytes(self):
         ret = []
 
-        ret.extend(b4(self.name))
+        ret.extend(b4(self.name_offset))
         ret.append(self.info)
         ret.append(self.other)
         ret.extend(b2(self.section_header_index))
@@ -156,6 +157,8 @@ class SymbolTableEntry(object):
         ret.extend(b8(self.size))
 
         return ret
+
+e_shoff = 388
 
 output = []
 
@@ -198,7 +201,7 @@ output.extend(b8(4194432))
 output.extend(b8(64))
 
 # +40 (8 bytes) e_shoff
-output.extend(b8(384))
+output.extend(b8(e_shoff))
 
 # +48 (4 bytes) e_flags
 output.extend([0, 0, 0, 0])
@@ -252,122 +255,163 @@ output.extend([0, 0, 0, 0, 0, 0, 0, 0])
 
 # program code ----------------------------------------------------------------
 
-# +128 (12 bytes) code section
-output.extend([0xb8, 0x3c, 0, 0, 0]) # mov rax, 60
-output.extend([0xbf, 0x4d, 0, 0, 0]) # mov rdi, 77
-output.extend([0x0f, 0x05])          # syscall
+# code + 0 (16 bytes) code section
+output.extend([0xb8, 0x3c, 0, 0, 0])    # mov rax, 60
+output.extend([0xbf, 0x4d, 0, 0, 0])    # mov rdi, 77
+output.extend([0x48, 0x83, 0xc7, 0x03]) # add rdi, 3
+output.extend([0x0f, 0x05])             # syscall
 
-# +140 (4 bytes) TODO: unknown
+# code + 16 (4 bytes) TODO: unknown
 output.extend([0, 0, 0, 0])
 
 # symtab ----------------------------------------------------------------------
 
-# +144 (24 bytes) symtab0
+# symtab + 0 (24 bytes) symtab0
 symtab0 = SymbolTableEntry()
-output.extend(symtab0.to_bytes())
 
-# +168 (24 bytes) symtab1
+# symtab + 24 (24 bytes) symtab1
 symtab1 = SymbolTableEntry()
 symtab1.info = STT_SECTION
 symtab1.section_header_index = 1
 symtab1.value = 4194432
-output.extend(symtab1.to_bytes())
 
-# +192 (24 bytes) symtab2
+# symtab + 48 (24 bytes) symtab2
 symtab2 = SymbolTableEntry()
-symtab2.name = 1 # exit77.asm
+symtab2.name = "exit77.asm"
 symtab2.info = STT_FILE
 symtab2.section_header_index = 65521
-output.extend(symtab2.to_bytes())
 
-# +216 (24 bytes) symtab3
+# symtab + 72 (24 bytes) symtab3
 symtab3 = SymbolTableEntry()
-symtab3.name = 17 # _start?
+symtab3.name = "_start"
 symtab3.info = 16 # ?
 symtab3.section_header_index = 1
 symtab3.value = 4194432
-output.extend(symtab3.to_bytes())
 
-# +240 (24 bytes) symtab4
+# symtab + 96 (24 bytes) symtab4
 symtab4 = SymbolTableEntry()
-symtab4.name = 12 # __bss_start
+symtab4.name = "__bss_start"
 symtab4.info = 16 # ?
 symtab4.section_header_index = 1
 symtab4.value = 6291596
-output.extend(symtab4.to_bytes())
 
-# +264 (24 bytes) symtab5
+# symtab + 120 (24 bytes) symtab5
 symtab5 = SymbolTableEntry()
-symtab5.name = 24 # _edata
+symtab5.name = "_edata"
 symtab5.info = 16 # ?
 symtab5.section_header_index = 1
 symtab5.value = 6291596
-output.extend(symtab5.to_bytes())
 
-# +288 (24 bytes) symtab6
+# symtab + 144 (24 bytes) symtab6
 symtab6 = SymbolTableEntry()
-symtab6.name = 31 # _end
+symtab6.name = "_end"
 symtab6.info = 16 # ?
 symtab6.section_header_index = 1
 symtab6.value = 6291600
-output.extend(symtab6.to_bytes())
+
+symtab = [
+    symtab0,
+    symtab1,
+    symtab2,
+    symtab3,
+    symtab4,
+    symtab5,
+    symtab6
+]
 
 # strtab ----------------------------------------------------------------------
 
-# +312 (1 byte)
-output.append(0)
+# Grab strings from symtab
+strtab = [st.name for st in symtab if st.name]
 
-# +313 (11 bytes) first string
-output.extend(bstr("exit77.asm"))
+# Deduplicate strings. If a string ends with another string it's considered
+# a duplicate since we can set an offset to partway through a string.
+duplicates = []
 
-# +324 (12 bytes) second string
-output.extend(bstr("__bss_start"))
+for si in range(len(strtab)):
+    s = strtab[si]
 
-# +336 (7 bytes) third string
-output.extend(bstr("_edata"))
+    for s2i in range(len(strtab)):
+        if si == s2i:
+            continue
 
-# +343 (5 bytes) fourth string
-output.extend(bstr("_end"))
+        s2 = strtab[s2i]
+
+        if s2.endswith(s):
+            if s not in duplicates:
+                duplicates.append(s)
+
+for d in duplicates:
+    strtab.remove(d)
+
+# Set offsets in symtab
+offset = 1
+
+for s in strtab:
+    for st in symtab:
+        if st.name is None or st.name_offset != 0:
+            continue
+
+        if s.endswith(st.name):
+            st.name_offset = offset + s.find(st.name)
+
+    offset += len(s) + 1
+
+# Render strtab binary
+strtab_bytes = [0]
+
+for s in strtab:
+    strtab_bytes.extend(bstr(s))
+
+# render symtab ---------------------------------------------------------------
+
+symtab_bytes = []
+
+for st in symtab:
+    symtab_bytes.extend(st.to_bytes())
+
+output.extend(symtab_bytes)
+output.extend(strtab_bytes)
 
 # shstrtab --------------------------------------------------------------------
 
-# +348 (1 byte) # TODO: unknown
+# shstrtab + 0 (1 byte) # TODO: unknown
 output.append(0)
 
-# +349 (1 byte) # TODO: unknown (all these 0x2e bytes)
+# shstrtab + 1 (1 byte) # TODO: unknown (all these 0x2e bytes)
 output.append(0x2e)
 
-# +350 (7 bytes)
+# shstrtab + 2 (7 bytes)
 output.extend(bstr("symtab"))
 
-# +357 (1 byte)
+# shstrtab + 9 (1 byte)
 output.append(0x2e)
 
-# +358 (7 bytes)
+# shstrtab + 10 (7 bytes)
 output.extend(bstr("strtab"))
 
-# +365 (1 byte)
+# shstrtab + 17 (1 byte)
 output.append(0x2e)
 
-# +366 (9 bytes)
+# shstrtab + 18 (9 bytes)
 output.extend(bstr("shstrtab"))
 
-# +375 (1 byte)
+# shstrtab + 27 (1 byte)
 output.append(0x2e)
 
-# +376 (5 bytes)
+# shstrtab + 28 (5 bytes)
 output.extend(bstr("text"))
 
-# +381 (3 bytes) # TODO: unknown
+# shstrtab + 33 (3 bytes) # TODO: unknown
 output.extend([0, 0, 0])
 
 # section headers -------------------------------------------------------------
 
-# +384 - 0x00 section header entry
+# e_shoff + 0 - 0x00 section header entry
 sh_zero = SectionHeader()
 output.extend(sh_zero.to_bytes())
 
-# +448 - 0x1b section header entry
+# e_shoff + 64 - 0x1b section header entry
 sh_text = SectionHeader()
 sh_text.name = 27
 sh_text.type = SHT_PROGBITS
@@ -378,22 +422,22 @@ sh_text.size = 12
 sh_text.addralign = 16
 output.extend(sh_text.to_bytes())
 
-# +512 - 0x11 section header entry
+# e_shoff + 128 - 0x11 section header entry
 sh_shstrtab = SectionHeader()
 sh_shstrtab.name = 17
 sh_shstrtab.type = SHT_STRTAB
 sh_shstrtab.flags = 0
 sh_shstrtab.addr = 0
-sh_shstrtab.offset = 348
+sh_shstrtab.offset = 352
 sh_shstrtab.size = 33
 sh_shstrtab.addralign = 1
 output.extend(sh_shstrtab.to_bytes())
 
-# +576 - 0x01 section header entry
+# e_shoff + 192 - 0x01 section header entry
 sh_symtab = SectionHeader()
 sh_symtab.name = 1
 sh_symtab.type = SHT_SYMTAB
-sh_symtab.offset = 144
+sh_symtab.offset = 148
 sh_symtab.size = 168
 sh_symtab.link = 4
 sh_symtab.info = 3
@@ -401,11 +445,11 @@ sh_symtab.addralign = 8
 sh_symtab.entsize = 24
 output.extend(sh_symtab.to_bytes())
 
-# +640 - 0x09 section header entry
+# e_shoff + 256 - 0x09 section header entry
 sh_strtab = SectionHeader()
 sh_strtab.name = 9
 sh_strtab.type = SHT_STRTAB
-sh_strtab.offset = 312
+sh_strtab.offset = 316
 sh_strtab.size = 36
 sh_strtab.addralign = 1
 output.extend(sh_strtab.to_bytes())
